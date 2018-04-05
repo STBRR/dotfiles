33cd833a71bec9773d302029ce662501a338a4c7
Liam <stabbxr@outlook.com>
2018-04-05 09:24:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 # dotfiles installation script ~ 11.11.2017
 #!/bin/bash
+clear
+echo "Stabber's Dotfile Script\n"
 
+echo "[*] Updating System & Installing Required Packages"
 sudo apt-get -y update
-sudo apt-get -y install htop ntpdate rofi i3 lolcat cowsay feh xcompmgr figlet ranger i3blocks pavucontrol pasystray >/dev/null 2>&1
+sudo apt-get -y install htop ntpdate rofi i3 lolcat cowsay feh xcompmgr figlet ranger i3blocks >/dev/null 2>&1
 
 # remove current timezone + set up system date time for uk time
 sudo rm /etc/localtime
@@ -17,7 +20,7 @@ cd out/
 sudo cp * /usr/share/fonts/ >/dev/null 2>&1
 sudo rm /etc/fonts/conf.d/70-no-bitmaps.conf
 sudo fc-cache -f -v >/dev/null 2>&1
-echo "[-] Tewi Installed"
+echo "[*] Tewi Installed"
 
 # download & install font awesome for icons
 cd && cd Downloads
@@ -28,7 +31,7 @@ sudo mkdir /usr/share/fonts/opentype/
 sudo cp * /usr/share/fonts/opentype/ >/dev/null 2>&1
 sudo rm /etc/fonts/conf.d/70*
 sudo fc-cache -f -v >/dev/null 2>&1
-echo "[-] Font Awesome Installed!"
+echo "[*] Font Awesome Installed!"
 
 # copy config files
 cd ~
@@ -36,8 +39,8 @@ cd && mkdir .i3
 cp dotfiles/i3/config  ~/.i3/
 cd && cd dotfiles; cd i3; sudo cp i3blocks.conf /etc/i3blocks.conf
 cd && cd dotfiles/bash/; sudo cp bashrc ~/.bashrc
-echo "[-] Configurations Installed!"
+echo "[*] Configurations Installed!"
 
 # clean up & reboot
 cd && rm -rf dotfiles/ Desktop/ Documents/ Downloads/ Music/ Pictures/ Public/ Templates/ Videos/; rm examples.desktop
-read -p "[-] Installation Successful!"; sudo reboot
+read -p "[!] Installation Successful!"; sudo reboot
