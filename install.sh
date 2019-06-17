d5ffa2f5775047a0bb08cee63eded7bf1e5135f6
Liam <stabbxr@outlook.com>
2019-06-17 19:46:06 +0100

    re-written install script with some new bits

--- install.sh
+++ install.sh
@@ -1,26 +1,48 @@
 #!/bin/bash
-echo "Stabber's Dotfile Script\n"
-echo "] Installing Packages"
-sudo apt-get -y update >/dev/null 2>&1
-sudo apt-get -y install htop ntpdate rofi i3 lolcat cowsay feh xcompmgr figlet ranger i3blocks fonts-font-awesome >/dev/null 2>&1
+# Updated: 17-06-2019 19:43
+echo "Liam's dotfile Installation"
 
-# install tewi-font from github
-git clone https://www.github.com/lucy/tewi-font.git >/dev/null 2>&1
-cd tewi-font
-make >/dev/null 2>&1
-cd out/
-sudo cp * /usr/share/fonts/ >/dev/null 2>&1
-sudo rm /etc/fonts/conf.d/70-no-bitmaps.conf >/dev/null 2>&1
-sudo fc-cache -f -v >/dev/null 2>&1
+echo "Installing Required Packages & Updating"
+# Update the system and install required packages from package manager.
+sudo apt -y update >/dev/null 2>&1
+sudo apt -y install htop zsh ntpdate rofi i3 lolcat cowsay feh xcompmgr figlet ranger i3blocks fonts-font-awesome thunar lxappearance gtk-chtheme qt4-qtconfig>/dev/null 2>&1
+echo "..success"
 
-# copy config files
-echo "] Copying Config Files"
-cd ~
-cd && mkdir .i3
-cp dotfiles/i3/config  ~/.i3/
-cd && cd dotfiles; cd i3; sudo cp i3blocks.conf /etc/i3blocks.conf
-cd && cd dotfiles/bash/; sudo cp bashrc ~/.bashrc
+echo "Installing PyWal"
+# Install pywal
+sudo pip3 install pywal > /dev/null 2>&1
+echo "..success"
 
-# clean up & reboot
-cd && rm -rf dotfiles/ Desktop/ Documents/ Downloads/ Music/ Pictures/ Public/ Templates/ Videos/; rm examples.desktop
-read -p "] Installation Successful!"; sudo reboot
+# Install SublimeText
+echo "Installing Sublime Text"
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt -y install apt-transport-https > /dev/null 2>&1
+sudo apt -y update > /dev/null 2>&1
+sudo apt -y install sublime-text
+echo "..success"
+
+# Install Tamsyn Font
+echo "Installing Tamsyn Font"
+wget "http://www.fial.com/~scott/tamsyn-font/download/tamsyn-font-1.11.tar.gz" -O /tmp/tamsyn-font.tar.gz > /dev/null 2>&1
+gunzip /tmp/t*.gz
+tar xvf t*.tar && cd tam*; sudo cp *.pcf /usr/share/fonts
+sudo rm /etc/fonts/conf.d/70-no*
+sudo fc-cache -f -v > /dev/null 2>&1
+echo "..success"
+
+# Install oh-my-zsh and set main shell to zsh on startup
+echo "Installing oh-my-zsh"
+echo "..current shell: $(echo $SHELL)"
+echo "..zsh Version: $(zsh --version)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" > /dev/null 2>&1
+chsh -s $(which zsh)
+echo "..success"
+
+# Copy i3 configuration
+echo "Copying Configuration Files"
+rm ~/.config/i3/config && cp ~/dotfiles/i3/config ~/.config/i3/config
+sudo cp ~/dotfiles/i3/i3blocks.conf /etc/i3blocks.conf
+rm ~/.zshrc; cp ~/dotfiles/zsh/zshrc ~/.zshrc
+echo "..success"
+
+echo "Installation Finished, Please reboot to finish setup."
