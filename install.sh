b5f04740f18106e9c615484b35390afc0e8a409b
Liam <stabbxr@outlook.com>
2020-02-14 16:15:55 -0500

    2020 baby

--- install.sh
+++ install.sh
@@ -1,53 +1,49 @@
 #!/bin/bash
-# Updated: 17-06-2019 19:43
-echo "Liam's dotfile Installation"
+echo "Dotfile Installation"
 
-echo "Installing Required Packages & Updating"
-# Update the system and install required packages from package manager.
+echo "..[1/6] - Updating Packages"
 sudo apt -y update >/dev/null 2>&1
-sudo apt -y install htop zsh ntpdate rofi i3 lolcat cowsay feh xcompmgr figlet ranger i3blocks fonts-font-awesome thunar lxappearance gtk-chtheme qt4-qtconfig>/dev/null 2>&1
 echo "..success"
 
-echo "Installing PyWal"
-# Install pywal
-sudo pip3 install pywal > /dev/null 2>&1
+echo "..[2/6] - Installing Packages"
+sudo cat packages.list | xargs -I {} sudo apt install -y {} 
+echo "..success"
+
+echo "..[3/6] - Installing PyWal"
+sudo pip3 install pywal
 echo "..success"
 
 # Install SublimeText
-echo "Installing Sublime Text"
+echo "[4/6] - Installing Sublime Text"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt -y install apt-transport-https > /dev/null 2>&1
-sudo apt -y update > /dev/null 2>&1
+sudo apt -y update
 sudo apt -y install sublime-text
 echo "..success"
 
-# Install Tamsyn Font
-echo "Installing Tamsyn Font"
-wget "http://www.fial.com/~scott/tamsyn-font/download/tamsyn-font-1.11.tar.gz" -O /tmp/tamsyn-font.tar.gz > /dev/null 2>&1
-gunzip /tmp/t*.gz
-tar xvf t*.tar && cd tam*; sudo cp *.pcf /usr/share/fonts
-sudo rm /etc/fonts/conf.d/70-no*
-sudo fc-cache -f -v > /dev/null 2>&1
-echo "..success"
-
 # Install oh-my-zsh and set main shell to zsh on startup
-echo "Installing oh-my-zsh"
+echo "[5/6] - Installing oh-my-zsh"
 echo "..current shell: $(echo $SHELL)"
 echo "..zsh Version: $(zsh --version)"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" > /dev/null 2>&1
-chsh -s $(which zsh)
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" > /dev/null 2>&1
 echo "..success"
 
 # Copy i3 configuration
-echo "Copying Configuration Files"
-rm ~/.config/i3/config && cp ~/dotfiles/i3/config ~/.config/i3/config
-sudo cp ~/dotfiles/i3/i3blocks.conf /etc/i3blocks.conf
+echo "[6/6] Copying Configuration Files"
 
 # Copy zsh configuration
-rm ~/.zshrc; cp ~/dotfiles/zsh/zshrc ~/.zshrc
+rm ~/.zshrc; cp configs/zshrc ~/.zshrc
 
-# Copy tmux configuration
-cp ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
-echo "..success"
+# Copy i3 config
+sudo rm ~/.configs/i3/config; cp configs/i3main ~/.configs/i3/config
+sudo cp configs/i3blocks /etc/i3blocks.conf
 
-echo "Installation Finished, Please reboot to finish setup."
+# Copy tmux config
+cp configs/tmux ~/.tmux.conf
+
+# Generate SSH keys
+echo "\nGenerating SSH Keys!"
+ssh-keygen
+
+chsh -s $(which zsh)
+echo "Installation Finished, Press Return to reboot"
+sudo reboot
